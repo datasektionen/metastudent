c7369a022fe79cd1bc8cf867d066eaa255ce5b56
haval0 <56519858+haval0@users.noreply.github.com>
2024-08-14 02:27:42 +0200

    add missing files

---
+++ src/pages/contact.js
@@ -0,0 +1,18 @@
+import {React} from "react";
+ 
+const Contact = () => {
+    return (
+        <main>
+            <h1>CS (Data) Chapter</h1>
+            <p>
+                isc@datasektionen.se
+            </p>
+            <h1>Media Chapter</h1>
+            <p>
+                international@medieteknik.com
+            </p>
+        </main>
+    );
+};
+ 
+export default Contact;
