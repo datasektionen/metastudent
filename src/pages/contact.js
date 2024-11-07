c7d87c5329e4b99e820fe88fc8819e6d7f49ba84
falu <falu@kth.se>
2024-11-07 12:02:31 +0100

    typos, clarification hyperlinks etc

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -24,7 +24,8 @@ const Contact = () => {
             <section className="in-tem">
                 <h2>International Committee Members</h2>
                 <ul>
-                    <li><span className="name">Håvard Alstadheim</span> <span className="role">- President</span></li>
+                    <li><span className="name">Håvard Alstadheim</span> <span className="role">- International Student Coordinator (ICS) for CS</span></li>
+                    <li><span className="name">Erik Sevelin and Elena Pan</span> <span className="role">- International Student Coordinator (ICS) for Media</span></li>
                     <li><span className="name">Dmitry Chirin, Håvard Alstadheim</span> <span className="role">- Head of events</span></li>
                     <li><span className="name">Rafael Oliveira, Elena Pan</span> <span className="role">- Head of communication</span></li>
                     <li><span className="name">Linn Benali</span> <span className="role">- Secretary</span></li>
@@ -45,7 +46,7 @@ const Contact = () => {
                 >
                     <h3>Who are you? Who is MAIN? What is a chapter?
                     </h3>
-                    {expanded === 0 && <p className="FAQ-answer">A chapter is a student union associated with a program at KTH, and for example the CS Chapter is then of course associated with the CS program. This website is run by the International Committee, shared between the two chapters for CS and Media. The main goal of the International Committee is to welcome international students into chapter life in general. Feel very free to contact us for more information. THS MAIN is an association that organizes events and more, similarly to chapters, but only for international and master’s students from all programs together.
+                    {expanded === 0 && <p className="FAQ-answer">A chapter is a student union associated with a program at KTH, and for example the Computer Science (CS) Chapter is then of course associated with the CS program. This website is run by the International Committee, shared between the two chapters for CS and Media (Media Technology). The main goal of the International Committee is to welcome international students into chapter life in general. Feel very free to contact us for more information. THS MAIN (Master's and International Group) is an association that organizes events and more, similarly to chapters, but only for international and master’s students from all programs together. Read more about THS MAIN here <a href="https://ths-main.mailchimpsites.com/" target="_blank" rel="noopener noreferrer">THS MAIN</a>.
 
 .</p>}
                 </div>
@@ -55,8 +56,8 @@ const Contact = () => {
                     onClick={() => toggleFAQ(1)}
                     aria-expanded={expanded === 1}
                 >
-                    <h3>How can I become active in the chapter and which clubs would you recommend?</h3>
-                    {expanded === 1 && <p className="FAQ-answer">Two good ways to start is to either go to an event or to join a club/committee (nämnd). The usual weekly chapter events don't require any signups, so just show up to one that seems interesting and start talking to someone there. Joining a group is usually done by contacting the club representative. We would recommend the clubs that are more “English friendly”, for example: The International Committee, Qulturnämnden, DESC, The Sports Committee and IOR..</p>}
+                    <h3>How can I become active in the chapter and which committees would you recommend?</h3>
+                    {expanded === 1 && <p className="FAQ-answer">Two good ways to start is to either go to an event or to join a committee (nämnd). The usual weekly chapter events don't require any signups, so just show up to one that seems interesting and start talking to someone there. Joining a group is usually done by contacting the committee representative. Committees that are mainly held in English is for example: The International Committee, Qulturnämnden, DESC, The Sports Committee and IOR. But in generall do all students speek very good English so don't hesitate to join other committees. You can find more info about the committees here <a href="https://datasektionen.se/namnder" target="_blank" rel="noopener noreferrer">Data Section Committees page</a>. </p>}
                 </div>
             </section>
         </main>
