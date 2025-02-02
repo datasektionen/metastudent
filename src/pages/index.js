561fc1ad6290680e378df9048240142d2cff1e5a
Alvinn8 <42838560+Alvinn8@users.noreply.github.com>
2025-02-02 21:59:12 +0100

    feat: Change font of frontpage paragraph

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,15 +7,10 @@ const Home = () => {
             <div className="row">
                 <div className="col-lg mx-3">
                     <h1>Welcome to META!</h1>
-                    <p>
+                    <p className="text-container">
                         META is the chapter hall shared between the Computer Science and Media Technology chapters at KTH.
                         This website is run by the International Committee, which is also a collaboration between the two
                         chapters.
-                    </p>
-                    <p>
-                        This website is extremely WIP at the moment.
-                        Don't hesitate to reach out to us at isc@datasektionen.se
-                        if you have any questions (or have a passion for web design...).
                     </p>
                 </div>
                 <div className="col-8-lg mx-auto">
